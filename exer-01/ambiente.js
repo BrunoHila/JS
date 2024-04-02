b1fdc631c8cb9a5f7ca181f4b7bac9e7b14ddae4
bruno hilario <brunohilario194@gmail.com>
2024-04-02 12:48:53 -0300

    atualizacao

--- exer-01/ambiente.js
+++ exer-01/ambiente.js
@@ -1,4 +1,10 @@
-for (var c = 1; c <= 10 ; c++) {
-  console.log(c);
+/*let num = [2,1,3]
+num.push(5)
+num[4]=4
+num.sort()  
+console.log(`seu vetor é ${num}`)*/
+
+var valores = [2,4,6,1,9], 
+for(let pos=0; pos < valores.length; pos++) { 
+  console.log(`a posicao ${pos} tem o valor ${valores[pos]}`)
 }
-console.log("finish");
